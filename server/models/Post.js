c0eb88c0c896eb30c2b2614ba588fbdfc20f55f3
sethukumar0001 <boylooksrock@gmail.com>
2019-06-25 11:48:12 +0530

    First_Commit

---
+++ server/models/Post.js
@@ -0,0 +1,17 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define collection and schema for Post
+let Post = new Schema({
+  title: {
+    type: String
+  },
+  body: {
+    type: String
+  }
+},{
+    collection: 'posts'
+});
+
+module.exports = mongoose.model('Post', Post);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import rootReducer from './reducers';
+import { fetchAllPosts } from './actions/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//import registerServiceWorker from './registerServiceWorker';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+store.dispatch(fetchAllPosts());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
+
+//registerServiceWorker();
